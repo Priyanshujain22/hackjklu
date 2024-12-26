a2fb8ea85555d1b137b3e25e49c4cfb64045ac00
Preet Taparia <preettaparia@jklu.edu.in>
2024-12-27 00:02:12 +0530

    updated team page

--- app/(main)/team/page.tsx
+++ app/(main)/team/page.tsx
@@ -1,8 +1,45 @@
+import Header from "@/components/Header/Header";
+import TeamCard from "@/components/Team/Team";
+import styles from "@/components/Team/Team.module.css"
+import teams from "@/data/teams.json"
+
+interface SocialLinks {
+  gb?: string;
+  email?: string;
+  linkedin?: string;
+}
+
+interface TeamCardProps {
+  name: string;
+  title: string;
+  socials: SocialLinks;
+  imageSrc: string;
+}
+
+const teamData: TeamCardProps[] = teams;
+
 
 export default function TeamPage() {
   return (
     <>
-    Team
+      <section className="px-10 md:px-20" >
+        <h2 className="text-center mb-10">
+          <Header text="Our Core Team" />
+        </h2>
+        <div className={`${styles.cardContainer}`}>
+          {teamData.map((item, index) => {
+            return (
+              <TeamCard
+                key={`${String(index)}-team`}
+                name={item.name}
+                title={item.title}
+                imageSrc={`/team/${item.imageSrc}.jpg`}
+                socials={item.socials}
+              />
+            );
+          })}
+        </div>
+      </section>
     </>
   );
 }
--- components/Team/Team.tsx
+++ components/Team/Team.tsx
@@ -1,10 +1,81 @@
 import React from 'react';
-// import styles from "./Team.module.css";
+import styles from "./Team.module.css";
+import Image from 'next/image';
+import { Mail, Github, Linkedin } from 'lucide-react';
 
-const Team = () => {
-  return (
-    <div>Team</div>
-  )
+const ICON_SIZE = 28;
+
+interface SocialLinks {
+  gb?: string;
+  email?: string;
+  linkedin?: string;
 }
 
-export default Team
+interface TeamCardProps {
+  name: string;
+  title: string;
+  socials: SocialLinks;
+  imageSrc: string;
+}
+
+const TeamCard: React.FC<TeamCardProps> = ({ name, title, socials, imageSrc }) => {
+  return (
+    <div className={styles.card}>
+      <div className="z-10 w-full text-center h-2/3 mb-18 mt-4 overflow-hidden rounded-lg">
+        <Image
+          className="z-10 rounded-lg object-cover object-top"
+          height="100"
+          width="100"
+          layout="responsive"
+          src={imageSrc}
+          alt={name}
+          quality={100}
+        />
+      </div>
+      <div className="z-10 text-center my-5">
+        <h3 className="text-xl">{name}</h3>
+        <h4>{title}</h4>
+      </div>
+      <div className={styles.socials}>
+        {socials.gb || socials.email || socials.linkedin ? (
+          <>
+            {socials.gb && (
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={socials.gb}
+              >
+                <Github size={ICON_SIZE} width={ICON_SIZE} />
+              </a>
+            )}
+
+            {socials.linkedin && (
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={socials.linkedin}
+              >
+                <Linkedin size={ICON_SIZE} />
+              </a>
+            )}
+            {socials.email && (
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={`mailto:${socials.email}`}
+              >
+                <Mail size={ICON_SIZE} />
+              </a>
+            )}
+          </>
+        ) : (
+          <span style={{ color: 'rgba(255,255,255,0.45)' }}>
+            Socially Invisible
+          </span>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default TeamCard
