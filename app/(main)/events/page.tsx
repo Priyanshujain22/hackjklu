37521b91cb38597e44231c37e9d1a60b1bd91e6e
Preet Taparia <preettaparia@jklu.edu.in>
2024-12-27 01:35:17 +0530

    updated events page

--- app/(main)/events/page.tsx
+++ app/(main)/events/page.tsx
@@ -1,15 +1,28 @@
 import Header from "@/components/Header/Header";
-import Event from "@/components/Events/Event";
+import EventCard from "@/components/Events/Event";
 import styles from "@/components/Events/Event.module.css";
+import eventsData from "@/data/events.json";
 
 export default function AboutPage() {
   return (
     <>
       <section className="px-10 md:px-20" >
         <h2 className="text-center mb-10">
-          <Header text="" />
+          <Header text="Events" />
         </h2>
 
+        <div className={styles.cardContainer}>
+          {eventsData.map((item, index) => {
+            return (
+              <EventCard
+                title={item.title}
+                description={item.description}
+                bgImage={item.bgImage}
+                key={index}
+              />
+            );
+          })}
+        </div>
       </section>
     </>
   );
--- components/Events/Event.tsx
+++ components/Events/Event.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { FC } from 'react';
+import styles from './Event.module.css';
 
-const Event = () => {
-  return (
-    <div>Event</div>
-  )
+interface EventCardProps {
+  title: string;
+  description: string;
+  bgImage?: string;
 }
 
-export default Event
+const EventCard: FC<EventCardProps> = ({ title, description, bgImage}) => {
+  return (
+    <div
+      style={{ backgroundImage: `url(${bgImage})` }}
+      className={styles.card}
+    >
+      <div className={styles.cardBody}>
+        <h2 className={styles.cardTitle}>{title}</h2>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default EventCard;
