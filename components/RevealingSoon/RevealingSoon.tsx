cf2736368574904fbf4ef7b5482efc9b090eef10
Preet Taparia <preettaparia@jklu.edu.in>
2025-01-02 20:49:28 +0530

    create revealingSoon copmonent and apply on Sponsers Section

---
+++ components/RevealingSoon/RevealingSoon.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from "./RevealingSoon.module.css";
+
+const RevealingSoon = () => {
+  return (
+    <div className={style.heading}>
+      <h1 className={style.headingText}>Revealing Soon</h1>
+    </div>  )
+}
+
+export default RevealingSoon
--- components/Sponsors/Sponsors.tsx
+++ components/Sponsors/Sponsors.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import RevealingSoon from '../RevealingSoon/RevealingSoon';
+import Header from '../Header/Header';
 // import styles from "./Sponsors.module.css";
 
 const Sponsors = () => {
   return (
-    <div>Sponsors</div>
-  )
+    <section className="p-8">
+      <h2 className="text-center mb-10">
+        <Header text="Sponsors" />
+      </h2>
+      <div className="py-10 flex justify-center items-center">
+        <RevealingSoon />
+      </div>
+    </section>
+  );
 }
 
-export default Sponsors
+export default Sponsors;
