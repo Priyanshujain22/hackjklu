3602cc367c9c0137d893eaaf910d8e41f17f06cd
Preet Taparia <preettaparia@jklu.edu.in>
2025-01-08 00:18:01 +0530

    updated heading

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import React from 'react';
 import style from "./Header.module.css";
+import ScrambleHover from './ScrambleHover';
 
 type HeaderProps = {
   text: string;
@@ -9,9 +12,16 @@ const Header = ({ text }: HeaderProps) => {
   
   return (
     <div className={style.heading}>
-      <h1 className={style.headingText}>{text}</h1>
-      <span />
-      <span />
+      <ScrambleHover
+          text={text}
+          scrambleSpeed={80}
+          sequential={false}
+          // revealDirection="center"
+          maxIterations={10}
+          useOriginalCharsOnly={false}
+          className={style.headingText}
+          characters="abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+-=[]{}|;':\,./<>?"
+        />
     </div>
   );
 };
