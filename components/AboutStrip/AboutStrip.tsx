cb3df9bba76773c8c1cb9024c7b59d9df985231e
Preet Taparia <preettaparia@jklu.edu.in>
2025-01-19 20:11:37 +0530

    update about strip

--- components/AboutStrip/AboutStrip.tsx
+++ components/AboutStrip/AboutStrip.tsx
@@ -2,39 +2,47 @@
 
 import { Linkedin, Instagram, Youtube } from "lucide-react";
 import Link from "next/link";
+import Image from "next/image";
+import JKLULogo from "@/public/jklu.png";
+import TechhLogo from "@/public/tech-affair.png";
 
 const AboutStrip = () => {
   return (
     <div className="relative">
-      <div className="mt-4 px-8 lg:px-16">
+      <div className="mt-4 px-8 lg:px-16 flex-col md:flex-row">
         <div className="flex flex-col sm:flex-row items-center sm:justify-between gap-8 sm:gap-0">
+          {/* Social media links */}
           <div className="flex gap-10 justify-center">
-            <Link
-              target="_blank"
-              href={"https://www.instagram.com/hackjklu"}
-            >
-              <Instagram size={40} className="hover:fill-neonGreen " />
+            <Link target="_blank" href={"https://www.instagram.com/hackjklu"}>
+              <Instagram size={40} className="hover:fill-neonGreen transition-all" />
             </Link>
-            <Link
-              target="_blank"
-              href={"https://www.youtube.com/@CouncilofTechnicalAffairs"}
-            >
-              <Youtube size={40} className="hover:fill-neonGreen "/>
+            <Link target="_blank" href={"https://www.youtube.com/@CouncilofTechnicalAffairs"}>
+              <Youtube size={40} className="hover:fill-neonGreen transition-all" />
             </Link>
-            <Link
-              target="_blank"
-              href={"https://www.linkedin.com/in/council-of-technical-affairs-jklu/"}
-            >
-              <Linkedin size={40} className="hover:fill-neonGreen " />
+            <Link target="_blank" href={"https://www.linkedin.com/in/council-of-technical-affairs-jklu/"}>
+              <Linkedin size={40} className="hover:fill-neonGreen transition-all" />
             </Link>
           </div>
 
-          <div className="flex gap-12 justify-center">
-            <div className="w-32 lg:w-40 text-center text-white">
-              <div className="w-full h-20 rounded">JKLU Logo</div>
+          {/* Logos section */}
+          <div className="flex flex-col sm:flex-row  gap-12 justify-center mt-8 sm:mt-0">
+            <div className="w-32 transform transition-all duration-300 hover:scale-105">
+              <Image
+                width={400}
+                height={400}
+                alt="JKLU"
+                src={JKLULogo}
+                className="object-contain"
+              />
             </div>
-            <div className="w-32 lg:w-40 text-center text-white">
-              <div className="w-full h-20 rounded">IET Logo</div>
+            <div className="w-72 transform transition-all duration-300 hover:scale-105">
+              <Image
+                width={400}
+                height={400}
+                alt="Tech Affair"
+                src={TechhLogo}
+                className="object-contain"
+              />
             </div>
           </div>
         </div>
