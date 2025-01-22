e7950faa64efc327504838e766281df9e3e5c92e
yashmish18 <yashmish10930@gmail.com>
2025-01-23 00:43:32 +0530

    update for themes about us itnereary yet to disclose for mobile browser

--- components/AboutStrip/AboutStrip.tsx
+++ components/AboutStrip/AboutStrip.tsx
@@ -10,28 +10,28 @@ const AboutStrip = () => {
   return (
     <div className="relative bg-gradient-to-b from-transparent via-black/50 to-black">
       <div className="mt-0 sm:mt-10 px-8 lg:px-16 flex-col md:flex-row">
-        <div className="flex flex-col sm:flex-row items-center sm:justify-between gap-4 sm:gap-0">
+        <div className="flex flex-col sm:flex-row items-center sm:justify-between gap-4 sm:gap-0 lg:gap-10">
           {/* Logos section */}
-          <div className="flex justify-between items-center w-full px-0 sm:justify-center order-1 sm:order-2">
-            <div className="transform transition-all duration-300 hover:scale-105 -ml-2">
+          <div className="flex justify-between items-center w-full px-0 sm:justify-center lg:justify-end order-1 sm:order-2">
+            <div className="transform transition-all duration-300 hover:scale-105 lg:mr-4">
               <a href="https://jklu.edu.in/council-of-technical-affairs/">
                 <Image
                   width={400}
                   height={400}
                   alt="Tech Affair"
                   src={TechhLogo}
-                  className="object-contain w-[120px] h-[80px] sm:w-[40px] sm:h-[40px] md:w-[50px] md:h-[50px] lg:w-[55px] lg:h-[55px]"
+                  className="object-contain w-[140px] h-[100px] sm:w-[40px] sm:h-[40px] md:w-[60px] md:h-[60px] lg:w-[200px] lg:h-[140px]"
                 />
               </a>
             </div>
-            <div className="transform transition-all duration-300 hover:scale-105">
+            <div className="transform transition-all duration-300 hover:scale-105 lg:ml-4">
               <a href="https://jklu.edu.in/">
                 <Image
                   width={400}
                   height={400}
                   alt="JKLU"
                   src={JKLULogo}
-                  className="object-contain w-[75px] h-[75px] sm:w-[40px] sm:h-[40px] md:w-[50px] md:h-[50px] lg:w-[55px] lg:h-[55px]"
+                  className="object-contain w-[75px] h-[75px] sm:w-[40px] sm:h-[40px] md:w-[50px] md:h-[50px] lg:w-[130px] lg:h-[130px]"
                 />
               </a>
             </div>
@@ -40,13 +40,13 @@ const AboutStrip = () => {
           {/* Social media links */}
           <div className="flex flex-col sm:flex-row gap-4 sm:gap-10 items-end mt-4 sm:mt-0 order-2 sm:order-1 w-full pr-2">
             <Link target="_blank" href={"https://www.instagram.com/hackjklu"}>
-              <Instagram size={25} className="hover:fill-neonGreen transition-all" />
+              <Instagram size={30} className="hover:fill-neonGreen transition-all" />
             </Link>
             <Link target="_blank" href={"https://www.youtube.com/@CouncilofTechnicalAffairs"}>
-              <Youtube size={25} className="hover:fill-neonGreen transition-all" />
+              <Youtube size={30} className="hover:fill-neonGreen transition-all" />
             </Link>
             <Link target="_blank" href={"https://www.linkedin.com/in/council-of-technical-affairs-jklu/"}>
-              <Linkedin size={25} className="hover:fill-neonGreen transition-all" />
+              <Linkedin size={30} className="hover:fill-neonGreen transition-all" />
             </Link>
           </div>
         </div>
