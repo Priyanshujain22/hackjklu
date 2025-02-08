9af9f033994b930cd44c5abcd0acd67ea078fa1e
Preet Taparia <preettaparia@jklu.edu.in>
2025-02-09 03:03:45 +0530

    update config

--- next.config.js
+++ next.config.js
@@ -7,3 +7,10 @@ module.exports = withBundleAnalyzer({
     formats: ["image/webp"],
   },
 });
+
+export function generateMetadata() {
+  return {
+    metadataBase: new URL('https://www.hackjklu.com'),
+  };
+}
+
