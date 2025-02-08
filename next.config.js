915b5874ef5d90e363f2f85c0ca9b8b5f8d00c05
Preet Taparia <preettaparia@jklu.edu.in>
2025-02-09 03:11:53 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-
-module.exports = withBundleAnalyzer({
+export default {
   images: {
     formats: ["image/webp"],
   },
-});
+};
 
 export function generateMetadata() {
   return {
-    metadataBase: new URL('https://www.hackjklu.com'),
+    metadataBase: new URL("https://www.hackjklu.com"),
   };
 }
-
